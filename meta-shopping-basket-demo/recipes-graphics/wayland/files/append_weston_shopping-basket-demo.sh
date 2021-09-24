79b3129f7151baea1d6375c5358769adde2f86e6
Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>
2021-09-24 10:35:00 +0100

    shopping-basket-demo: Set weston resolution to 720
    The weston.ini configuration sets the resolution of the HDMI mode to
    1280x720.
    Signed-off-by: Ayman Chaudhry <ayman.chaudhry.kc@renesas.com>

--- meta-shopping-basket-demo/recipes-graphics/wayland/files/append_weston_shopping-basket-demo.sh
+++ meta-shopping-basket-demo/recipes-graphics/wayland/files/append_weston_shopping-basket-demo.sh
@@ -5,6 +5,10 @@ SHOPPING_BASKET_INSTALL_DIR=${2}
 
 cat >> "${WESTON_FILE}" <<-EOF
 
+[output]
+name=HDMI-A-1
+mode=1280x720@60.0
+
 [launcher]
 icon=${SHOPPING_BASKET_INSTALL_DIR}/icons/shopping-basket-demo.png
 path=${SHOPPING_BASKET_INSTALL_DIR}/scripts/shopping-basket-demo.sh
